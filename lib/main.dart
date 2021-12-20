b9380e0fb4b91d11316f69c18d11b5b43b66d0d2
Grayson <graysoncrozier40@gmail.com>
2021-12-19 21:32:27 -0500

    Add platform specific firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:p2p_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
